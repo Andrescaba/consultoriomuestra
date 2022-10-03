bad3b6cde00e1606e3c95b53cc2585846be12d41
Andrescaba <93167157+Andrescaba@users.noreply.github.com>
2022-10-03 05:24:03 -0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,3 @@
+function mialerta() {  
+            alert("Esta es una página ilustrativa para mostrar el diseño de la web, no representa un lugar medico real ni doctores reales. Creada por Andres Caballero. Vea la seccion de contacto o visite www.linkedin.com/in/andres-caballero-67829b24a para mas informacion ");
+        } 
